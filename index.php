9ab37c273aed0ea2bf30611cf4f0fc3fff20822b
Elaine <118221962@umail.ucc.ie>
2019-02-26 19:23:23 +0000

    all submits

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Milti-page Form-Page One</title>
+
+</head>
+
+
+<body>
+
+<p>Please fill in the following information</p>
+
+<Form method="POST" ACTION="page2.php">
+Name:<INPUT TYPE="text" SIZE="40" name="cust_name"><BR>
+Email:<INPUT TYPE="text" SIZE="40" name="cust_name"><BR>
+<INPUT TYPE="submit" name="submit1" value="Proceed">
+</Form>
+	
+</body>
+
+
+</html>
---
+++ page2.php
@@ -0,0 +1,27 @@
+<?php
+ $cust_name=htmlentities($_POST['cust_name']); 
+ $cust_email=htmlentities($_POST['cust_email']);
+ 
+ ?> 
+ 
+ <HTML>
+ <HEAD>
+ <TITLE>Multi-page Form-Page Two</TITLE>
+ </HEAD>
+ <BODY>
+ 
+ <p>Please fill in the following information</p>
+ 
+ <FORM METHOD="POST" ACTION="final.php">
+ 
+ Address:<INPUT TYPE="text" SIZE="50" name="cust_address"><br/>
+ Phone:<INPUT TYPE="text" SIZE="20" name="cust_phone"><br/>
+ <INPUT TYPE="hidden" name="cust_name"
+ VALUE="<?php echo $cust_name>">
+ <INPUT TYPE="hidden" name="cust_email"
+ VALUE="<?php echo $cust_email;?>">
+ <INPUT TYPE="submit" name="submit2" value="Proceed">
+ 
+ </FORM>
+ </BODY>
+ </HTML>
